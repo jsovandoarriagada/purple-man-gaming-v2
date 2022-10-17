5b672462c488ed5b1341e4d082580cb658fc8287
José Simón Ovando Arriagada <jsovandoarriagada@gmail.com>
2022-10-17 12:51:23 -0300

    Fix FormModal title and make every input required

--- src/components/FormModal/FormModal.js
+++ src/components/FormModal/FormModal.js
@@ -4,12 +4,12 @@ function FormModal({ openModal, createOrder }) {
   return (
     <div className="modal">
       <div className="modal__container">
-        <p className="modal__title">👉 PLEASE, FILL IN ALL FIELDS</p>
+        <p className="modal__title">PLEASE, FILL IN ALL FIELDS</p>
         <form className="modal__form">
-          <input type="text" id="name" placeholder="Your name" autoComplete="off"></input>
-          <input type="tel" id="phoneNumber" placeholder="Your phone number" autoComplete="off"></input>
-          <input type="email" id="email" placeholder="Your email" autoComplete="off"></input>
-          <input type="email" id="confirmEmail" placeholder="Confirm your email" autoComplete="off"></input>
+          <input type="text" id="name" placeholder="Your name" autoComplete="off" required></input>
+          <input type="tel" id="phoneNumber" placeholder="Your phone number" autoComplete="off" required></input>
+          <input type="email" id="email" placeholder="Your email" autoComplete="off" required></input>
+          <input type="email" id="confirmEmail" placeholder="Confirm your email" autoComplete="off" required></input>
         </form>
         <div className="modal__buttons">
           <button className="modal__proceed" onClick={createOrder}>
