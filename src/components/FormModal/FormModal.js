31351fd2fc67e3d130f3ede7d2e9e833bb777cac
José Simón Ovando Arriagada <jsovandoarriagada@gmail.com>
2022-10-17 13:01:53 -0300

    Add form validation on Cart

--- src/components/FormModal/FormModal.js
+++ src/components/FormModal/FormModal.js
@@ -1,24 +1,56 @@
 import "./FormModal.css";
 
-function FormModal({ openModal, createOrder }) {
+function FormModal({ userInfo, setModal, handleChange, handleProceed }) {
   return (
     <div className="modal">
       <div className="modal__container">
         <p className="modal__title">PLEASE, FILL IN ALL FIELDS</p>
         <form className="modal__form">
-          <input type="text" id="name" placeholder="Your name" autoComplete="off" required></input>
-          <input type="tel" id="phoneNumber" placeholder="Your phone number" autoComplete="off" required></input>
-          <input type="email" id="email" placeholder="Your email" autoComplete="off" required></input>
-          <input type="email" id="confirmEmail" placeholder="Confirm your email" autoComplete="off" required></input>
+          <input
+            type="text"
+            id="name"
+            placeholder="Your name"
+            onChange={handleChange}
+            value={userInfo.name}
+            autoComplete="off"
+            required
+          ></input>
+          <input
+            type="tel"
+            id="phoneNumber"
+            placeholder="Your phone number"
+            onChange={handleChange}
+            value={userInfo.phoneNumber}
+            autoComplete="off"
+            required
+          ></input>
+          <input
+            type="email"
+            id="email"
+            placeholder="Your email"
+            onChange={handleChange}
+            value={userInfo.email}
+            autoComplete="off"
+            required
+          ></input>
+          <input
+            type="email"
+            id="confirmEmail"
+            placeholder="Confirm your email"
+            onChange={handleChange}
+            value={userInfo.confirmEmail}
+            autoComplete="off"
+            required
+          ></input>
         </form>
         <div className="modal__buttons">
-          <button className="modal__proceed" onClick={createOrder}>
+          <button className="modal__proceed" onClick={handleProceed}>
             PROCEED
           </button>
           <button
             className="modal__cancel"
             onClick={() => {
-              openModal(false);
+              setModal(false);
             }}
           >
             CANCEL
