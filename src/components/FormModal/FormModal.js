cf80358fa19d017cddc8429f6192b887d8ee7f12
José Simón Ovando Arriagada <jsovandoarriagada@gmail.com>
2022-10-16 22:59:28 -0300

    Fix FormModal styles

--- src/components/FormModal/FormModal.js
+++ src/components/FormModal/FormModal.js
@@ -4,12 +4,12 @@ function FormModal({ openModal, createOrder }) {
   return (
     <div className="modal">
       <div className="modal__container">
-        <p className="modal__title">BILLING INFORMATION</p>
+        <p className="modal__title">👉 PLEASE, FILL IN ALL FIELDS</p>
         <form className="modal__form">
-          <input type="text" id="name" placeholder="NAME" autoComplete="off"></input>
-          <input type="tel" id="phoneNumber" placeholder="PHONE NUMBER" autoComplete="off"></input>
-          <input type="email" id="email" placeholder="EMAIL" autoComplete="off"></input>
-          <input type="email" id="confirmEmail" placeholder="CONFIRM EMAIL" autoComplete="off"></input>
+          <input type="text" id="name" placeholder="Your name" autoComplete="off"></input>
+          <input type="tel" id="phoneNumber" placeholder="Your phone number" autoComplete="off"></input>
+          <input type="email" id="email" placeholder="Your email" autoComplete="off"></input>
+          <input type="email" id="confirmEmail" placeholder="Confirm your email" autoComplete="off"></input>
         </form>
         <div className="modal__buttons">
           <button className="modal__proceed" onClick={createOrder}>
